99857f38a84bd13863a297d8adb4a912a9468386
zhuguiquan <@137478494qq.com>
2019-11-29 18:50:50 +0800

    sss

--- app/src/main/java/com/bw/dianshangday1_one/Fragment2.java
+++ app/src/main/java/com/bw/dianshangday1_one/Fragment2.java
@@ -1,6 +1,8 @@
 package com.bw.dianshangday1_one;
 
+import android.content.Intent;
 import android.view.View;
+import android.widget.Button;
 
 /**
  * DateTime:2019/11/29 0029
@@ -8,6 +10,9 @@ import android.view.View;
  * function:
  */
 public class Fragment2 extends BaseFragment {
+
+    private Button button;
+
     @Override
     protected void initData() {
 
@@ -15,6 +20,14 @@ public class Fragment2 extends BaseFragment {
 
     @Override
     protected void initView(View inflate) {
+        button = inflate.findViewById(R.id.bt);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
 
     }
 
